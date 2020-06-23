98609d0f555d41c30e177a06bd9e7b19985027a4
Nathaniel F <nathaniel.fitzenrider@gmail.com>
2020-06-23 13:51:37 -0400

    Add remove_aestd.sh

---
+++ remove_aestd.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2020 Nathaniel Fitzenrider
+#
+# SPDX-License-Identifier: MIT
+
+shopt -s extglob
+
+rm -rf data/aestd.*/
+
+rm -rf -- data/aestd1/tags/!(functions)
+rm -rf -- data/aestd1/!(tags)
+rm -rf data/load/
+rm -f data/minecraft/loot_tables/blocks/yellow_shulker_box.json
+rmdir --ignore-fail-on-non-empty -p data/minecraft/loot_tables/blocks
