a669007b3a081e1c192c38cfd8acf51d1eb1d68b
Nathaniel F <nathaniel.fitzenrider@gmail.com>
2020-06-23 13:55:23 -0400

    Update AESTD management scripts

--- merge_aestd.sh
+++ merge_aestd.sh
@@ -9,11 +9,10 @@ rm -rf data/aestd.*/
 
 rm -rf -- data/aestd1/tags/!(functions)
 rm -rf -- data/aestd1/!(tags)
-rm -rf data/load/
 rm -f data/minecraft/loot_tables/blocks/yellow_shulker_box.json
 rmdir --ignore-fail-on-non-empty -p data/minecraft/loot_tables/blocks
 
 cp -rn AESTD/data/aestd* data/
 rm -rf data/aestd.tools/ # kinda inefficient but whatever
-cp -r AESTD/data/load/ data/
+cp -rn AESTD/data/load/ data/
 cp -r AESTD/data/minecraft/loot_tables/ data/minecraft/
--- remove_aestd.sh
+++ remove_aestd.sh
@@ -9,6 +9,5 @@ rm -rf data/aestd.*/
 
 rm -rf -- data/aestd1/tags/!(functions)
 rm -rf -- data/aestd1/!(tags)
-rm -rf data/load/
 rm -f data/minecraft/loot_tables/blocks/yellow_shulker_box.json
 rmdir --ignore-fail-on-non-empty -p data/minecraft/loot_tables/blocks
