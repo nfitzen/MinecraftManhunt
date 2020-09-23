2b7d2ea85daa8324892c63e21bc7889e009f5a47
Nathaniel F <nathaniel.fitzenrider@gmail.com>
2020-09-23 17:55:12 -0400

    Add auto-versioning to build.sh & clean up licensing

--- build.sh
+++ build.sh
@@ -1,30 +1,43 @@
 #!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2020 Nathaniel Fitzenrider <https://github.com/nfitzen>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: CC0-1.0
+
+PACK_NAME="MinecraftManhunt"
+
+if command -v zip; then
+    ZIP="zip -r";
+else
+    ZIP="7z a";
+fi
+
+version=$(git describe --tags --dirty)
 
 rm -r .build/
 mkdir .build releases .build/files
 
 cp -r data/ ./pack.mcmeta .build/files/
 
-cp -r libs/AESTD/ .build/
-
 cp -r .reuse/ LICENSE LICENSES/ README.md .build/files
 
-cd .build
+# Currently only implemented for AESTD
 
-cp -rn AESTD/data/aestd*/ files/data
+mkdir .build/libs
+cp -r libs/AESTD/ .build/libs/
+
+cd .build/
+
+cp -rn libs/AESTD/data/aestd*/ files/data/
 rm -rf files/data/aestd.tools/ # kinda inefficient but whatever
-cp -rn AESTD/data/load/ files/data/
-cp -r AESTD/data/minecraft/loot_tables/ files/data/minecraft/
+cp -rn libs/AESTD/data/load/ files/data/
+cp -r libs/AESTD/data/minecraft/loot_tables/ files/data/minecraft/
 
-cd files
+cd files/
 
-rm ../../releases/MinecraftManhunt-$(git describe --tags --dirty).zip
+# a hacky workaround since `>` overwrites the file before it's read
+(version=$version envsubst < ./pack.mcmeta) > ./pack.mcmeta.tmp && \
+    mv ./pack.mcmeta.tmp ./pack.mcmeta
 
-zip -r ../../releases/MinecraftManhunt-$(git describe --tags --dirty).zip data/ LICENSE LICENSES/ pack.mcmeta README.md
+rm ../../releases/$PACK_NAME-$version.zip
 
-# cd ../..
-
-# cp .build/MinecraftManhunt.zip releases/
+$ZIP ../../releases/$PACK_NAME-$version.zip **
