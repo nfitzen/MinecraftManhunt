027756e9c95174d596be82b43906a97ccd42ec61
Nathaniel Fitzenrider <nathaniel@fitzenrider.com>
2021-04-09 19:21:07 -0400

    Tweak auto copyright notice in gen_set_size.py

--- data/nlib/functions/worldborder/gen_set_size.py
+++ data/nlib/functions/worldborder/gen_set_size.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2020 Nathaniel Fitzenrider <https://github.com/nfitzen>
+# SPDX-FileCopyrightText: 2020-2021 Nathaniel Fitzenrider <https://github.com/nfitzen>
 #
 # SPDX-License-Identifier: MIT
+
+from datetime import datetime
+
+year = datetime.date().today().year
 
 # 60,000,000 == 0b00000011100100111000011100000000
 # 26 bits
@@ -10,10 +14,10 @@ bits = range(25,-1,-1)
 
 tests = (2**x for x in bits)
 
-year = 2020
+year = "2020-" + year
 copyrightHolder = "Nathaniel Fitzenrider"
 contact = "<https://github.com/nfitzen>"
-license = "MIT"
+l = "MIT"
 
 spdx = 'SPDX-' # ensures automatic SPDX tools don't get it mixed up
 
@@ -22,7 +26,7 @@ filename = 'set_size.mcfunction'
 with open(filename, 'w') as f:
     start = f"""# {spdx}FileCopyrightText: {year} {copyrightHolder} {contact}
 #
-# {spdx}License-Identifier: {license}
+# {spdx}License-Identifier: {l}
 
 worldborder set 1
 
